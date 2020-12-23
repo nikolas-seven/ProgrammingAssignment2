59982dd0cf508c34119f155a718a94b0ba1995bd
Alexandre Nikolas <alenikolas1@gmail.com>
2020-12-23 13:16:32 +0100

    Second programming assignement

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The aim of this project is to verify if the inverse of a matrix has
+## already been calculated in the environment and if so, it will not 
+## calculate it again
 
-## Write a short comment describing this function
+## It creates a special vector that has a function to do the following:
+## set and get the value of a matrix
+## and set and get the value of it's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of a matrix by first checking if
+## it has already been calculated. If so, it skips the calculation and provides
+## the inverse matrix
 
 cacheSolve <- function(x, ...) {
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
         ## Return a matrix that is the inverse of 'x'
 }
